4d82f4d55bece2130f012c5aff8ea46d080ea589
konglong <1016361268@qq.com>
2022-07-09 12:46:30 +0800

    省市区组件初始化并在github上拿到省市区json文件到项目中

---
+++ src/components/chooseArea/src/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-select placeholder="请选择省份" v-model="province">
+      <el-option value="1">1</el-option>
+    </el-select>
+    <el-select style="margin: 0 10px;" placeholder="请选择城市" v-model="city">
+      <el-option value="1">2</el-option>
+    </el-select>
+    <el-select placeholder="请选择区域" v-model="area">
+      <el-option value="1">3</el-option>
+    </el-select>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import allAreas from './lib/pca-code.json'
+import { ref } from "vue"
+
+console.log(allAreas)
+let province = ref<string>('')
+let city = ref<string>('')
+let area = ref<string>('')
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/chooseArea/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <choose-area></choose-area>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs, ref } from "vue" 
+import ChooseArea from '../../components/chooseArea/src/index.vue'
+  
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
