6b1a31c1914cfd5ada6991b057388cd57a597c22
konglong <1016361268@qq.com>
2022-07-09 09:26:16 +0800

    完成伸缩菜单组件

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -1,33 +1,11 @@
 <template>
   <el-container>
     <el-aside width="auto">
-      <el-menu default-active="2" :collapse="isCollapse" class="el-menu-vertical-demo">
-        <el-menu-item index="1">
-          <el-icon>
-            <component is="el-icon-menu"></component>
-          </el-icon>
-          <span>导航1</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <el-icon>
-            <component is="el-icon-menu"></component>
-          </el-icon>
-          <span>导航2</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <el-icon>
-            <component is="el-icon-menu"></component>
-          </el-icon>
-          <span>导航3</span>
-        </el-menu-item>
-      </el-menu>
+      <nav-side :collapse="isCollapse"></nav-side>
     </el-aside>
     <el-container>
       <el-header>
-        <span @click="toggle">
-          <el-icon-expand v-if="isCollapse"></el-icon-expand>
-          <el-icon-fold v-else></el-icon-fold>
-        </span>
+        <nav-header v-model:collapse="isCollapse"></nav-header>
       </el-header>
       <el-main>
         <router-view></router-view>
@@ -37,17 +15,17 @@
 </template>
  
 <script setup lang="ts">
-import { reactive, toRefs, ref } from "vue"
+import { ref } from "vue"
+import NavHeader from './navHeader/index.vue'
+import NavSide from './navSide/index.vue'
 let isCollapse = ref(false)
 
-let toggle = () => {
-  isCollapse.value = !isCollapse.value
-}
+
 </script>
 
 <style lang="scss" scoped>
-// 如果菜单没有收缩就定为宽度200
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
+.el-header {
+  padding: 0;
+  border-bottom: 1px solid #eee;
 }
 </style>
---
+++ src/components/container/src/navSide/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-menu default-active="2" :collapse="collapse" class="el-menu-vertical-demo">
+    <el-menu-item index="1">
+      <el-icon>
+        <component is="el-icon-menu"></component>
+      </el-icon>
+      <span>首页</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon>
+        <component is="el-icon-menu"></component>
+      </el-icon>
+      <span>图标选择器</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon>
+        <component is="el-icon-menu"></component>
+      </el-icon>
+      <span>趋势标记</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  collapse: boolean
+}>()
+</script>
+
+<style lang="scss" scoped>
+// 如果菜单没有收缩就定为宽度200
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+}
+</style>
