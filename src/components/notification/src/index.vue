6b41e94c78d8f12a6203ea7a2bef1c53c69e49d7
konglong <1016361268@qq.com>
2022-07-09 17:16:40 +0800

    通知菜单初始化与静态样式编写

---
+++ src/components/notification/src/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-badge style="cursor: pointer;" :value="value" :max="max" :is-dot="isDot">
+    <component :is="`el-icon-${toLine(icon)}`"></component>
+  </el-badge>
+</template>
+
+<script setup lang="ts">
+import { toLine } from '../../../utils'
+import { reactive, toRefs, ref } from "vue"
+let props = defineProps({
+  // 图标
+  icon: {
+    type: String,
+    default: 'Bell'
+  },
+  // 通知数量
+  value: {
+    type: [String, Number],
+    default: ''
+  },
+  // 最大值
+  max: {
+    type: Number,
+  },
+  // 是否显示小圆点
+  isDot: {
+    type: Boolean,
+    default: false
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/notification/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <notification :value="50" />
+  <br />
+  <br />
+  <notification :value="50" :max="30" />
+  <br />
+  <br />
+  <notification :value="50" :isDot="true" />
+  <br />
+  <br />
+  <notification icon="ChatDotRound" :value="50" :isDot="true" />
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs, ref } from "vue"
+
+</script>
+
+<style lang="scss" scoped>
+</style>
