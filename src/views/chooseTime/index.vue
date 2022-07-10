257485cb8bd89bde0d5b6eaeec783d4205ce1127
konglong <1016361268@qq.com>
2022-07-10 11:17:25 +0800

    完成时间选择组件封装

---
+++ src/views/chooseTime/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <choose-time @startChange="startChange" @endChange="endChange" />
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs, ref } from "vue"
+
+interface endValue {
+  startTime: string,
+  endTime: string
+}
+
+let startChange = (val: string) => {
+  console.log('startTime', val)
+}
+let endChange = (val: endValue) => {
+  console.log('endTime', val)
+}
+
+// 测试传入子组件未用props定义的属性
+// let startOptions = {
+//   size: 'small',
+//   clearable: false
+// }
+
+</script>
+
+<style lang="scss" scoped>
+</style>
