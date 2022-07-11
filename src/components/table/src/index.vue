c509444bb4d35f190ffacfe4b897c658275b40ce
konglong <1016361268@qq.com>
2022-07-11 15:28:17 +0800

    定义表格组件需要的数据类型

---
+++ src/components/table/src/index.vue
@@ -0,0 +1,24 @@
+<template>
+  table
+</template>
+
+<script setup lang="ts">
+import { PropType, ref } from "vue"
+import { TableOptions } from "./types"
+
+let props = defineProps({
+  // 表格的配置
+  options: {
+    type: Array as PropType<TableOptions[]>,
+    require: true
+  },
+  // 表格数据
+  data: {
+    type: Array as PropType<any[]>,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/table/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <my-table></my-table>
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs, ref } from "vue"
+
+</script>
+
+<style lang="scss" scoped>
+</style>
