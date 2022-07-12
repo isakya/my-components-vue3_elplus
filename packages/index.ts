d33bf5e7ec45b35383b1672db4ad452d25a591fa
konglong <1016361268@qq.com>
2022-07-12 13:12:55 +0800

    更新npm包

--- packages/index.ts
+++ packages/index.ts
@@ -15,6 +15,8 @@ import table from './table'
 import calendar from './calendar'
 import './styles/base.scss'
 import './styles/ui.scss'
+import * as Icons from '@element-plus/icons-vue'
+import {toLine} from './utils'
 
 const components  = [
   chooseArea,
@@ -36,7 +38,14 @@ const components  = [
 // 批量注册组件
 export default {
   install(app: App) {
+    for(let i in Icons) {
+      // 全局注册图标 牺牲一点性能
+      // 改造成 el-icon-xxx 名字
+      // 注册全局组件
+      app.component(`el-icon-${toLine(i)}`, (Icons as any)[i])
+    }
     components.map(item => {
+
       app.use(item)
     })
   }
