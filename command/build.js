5f8ff11e77e9115c791edf25b8ce7d9ce2a65d7b
konglong <1016361268@qq.com>
2022-07-12 11:41:32 +0800

    完成单独打包每个组件并实现按需引入

--- command/build.js
+++ command/build.js
@@ -3,6 +3,8 @@ const path = require('path')
 const { defineConfig, build } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
+const fsExtra = require('fs-extra')
+const fs = require('fs')
 
 // 打包入口文件夹
 const entryDir = path.resolve(__dirname, '../packages')
@@ -43,9 +45,58 @@ const buildAll = async () => {
   })
 }
 
+// 单组件打包构建
+// name: 组件名称
+const buildSingle = async (name) => {
+  await build({
+    ...baseConfig,
+    build: {
+      rollupOptions,
+      lib: {
+        entry: path.resolve(entryDir, name),
+        name: 'index',
+        fileName: 'index',
+        formats: ['es', 'umd']
+      },
+      outDir: path.resolve(outDir, name)
+    }
+  })
+}
+
+// 每个组件生成package.json文件
+const createPackageJson = (name) => {
+  const fileStr = `
+  {
+    "name": "${name}",
+    "main": "index.umd.js",
+    "module": "index.es.js"
+    "style": "style.css"
+  }`
+  // 输出
+  fsExtra.outputFile(
+    path.resolve(outDir, `${name}/package.json`),
+    fileStr,
+    'utf-8'
+  )
+}
+
 // 打包成库
 const buildLib = async () => {
   await buildAll()
+
+  // 获取组件名称组成的数组
+  const components = fs.readdirSync(entryDir).filter(name => {
+    const componentDir = path.resolve(entryDir, name)
+    // 判断是否为目录
+    const isDir = fs.lstatSync(componentDir).isDirectory()
+    return isDir && fs.readdirSync(componentDir).includes('index.ts')
+  })
+
+  // 循环构建
+  for (const name of components) {
+    await buildSingle(name)
+    createPackageJson(name)
+  }
 }
 
 buildLib()
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,21 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as Icons from '@element-plus/icons-vue'
 import {toLine} from './utils'
-// import myUI from './components'
-import myUI from '../lib/isakya-element-components.es'
-import '../lib/style.css'
+import myUI from './components'
+
+// 全部引入组件测试
+// import myUI from '../lib/isakya-element-components.es'
+// import '../lib/style.css'
+
+// 单独引入组件测试
+// import chooseIcon from '../lib/chooseIcon/index.es'
+// import   '../lib/chooseIcon/style.css'
+
+
 import './mock'
+
+
+
 const app = createApp(App)
 
 // 全局注册图标 牺牲一点性能
@@ -18,5 +29,9 @@ for(let i in Icons) {
   app.component(`el-icon-${toLine(i)}`, (Icons as any)[i])
 }
 
-app.use(router).use(ElementPlus).use(myUI)
+app.use(router)
+.use(ElementPlus)
+// 测试单独引入
+// .use(chooseIcon)
+.use(myUI)
 app.mount('#app')
