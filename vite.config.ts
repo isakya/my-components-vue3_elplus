1de728efa96fa3ea895cee06de71ec28f7b8d43b
konglong <1016361268@qq.com>
2022-07-12 18:45:33 +0800

    打包拆分css和js

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,27 @@ export default defineConfig({
   plugins: [vue(),vueJsx()],
   server: {
     port: 8080
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          var info = assetInfo.name.split('.')
+          var extType = info[info.length - 1]
+          if (
+            /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)
+          ) {
+            extType = 'media'
+          } else if (/\.(png|jpe?g|gif|svg)(\?.*)?$/.test(assetInfo.name)) {
+            extType = 'img'
+          } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
+            extType = 'fonts'
+          }
+          return `static/${extType}/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js'
+      }
+    }
   }
 })
